8709caa8b197f30397d283ca6513f78752710afd
DanniKV <dannikvase@hotmail.com>
2018-10-08 18:37:53 +0200

    Ekstra Packages
    + stuff for CoffeeContext

--- CoffeeShop.Infrastructure.Data/Repositories/CoffeeContext.cs
+++ CoffeeShop.Infrastructure.Data/Repositories/CoffeeContext.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Text;
 using CoffeeShop.Core.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CoffeeShop.Infrastructure.Data.Repositories
 {   //Connection Point to the Database, only contains parts of the DB
     //Only contains content that we are working with at a given time
-    public class CoffeeContext
+    public class CoffeeContext : DbContext
     {
+        //SuperClass 
+        //Options for database: Sql, in-Memory and so on...
+        public CoffeeContext(DbContextOptions<CoffeeContext> Opt) : base(Opt)
+        {
 
+        }
+        //Fluint API Model-Builder
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+        }
+        public DbSet<Coffee> Customers { get; set; }
     }
+    
 }
